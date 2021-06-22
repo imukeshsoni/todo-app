6574f7abfeba422fa324f29d3142b6c493914009
Mukesh <mksoni@arinspect.com>
2021-06-22 12:16:10 +0530

    todo model created

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/model/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'This is a title',
+        desc: 'This is description',
+        active: true,
+      },
+      {
+        sno: 2,
+        title: 'This is a title',
+        desc: 'This is description',
+        active: true,
+      },
+      {
+        sno: 3,
+        title: 'This is a title',
+        desc: 'This is description',
+        active: true,
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }
