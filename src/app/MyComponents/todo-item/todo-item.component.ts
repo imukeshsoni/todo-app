33ac5191dc4c169eea49848a7a20ff360076fc6c
Mukesh <mksoni@arinspect.com>
2021-06-22 16:45:29 +0530

    added todo done feature

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -9,7 +9,10 @@ import { Todo } from 'src/app/model/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo:Todo;
+  @Input() i:number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +20,10 @@ export class TodoItemComponent implements OnInit {
   onClick(todo: Todo){
     this.todoDelete.emit(todo);
     console.log("Button clicked")
+  }
+  onCheckboxClick(todo){
+    this.todoCheckbox.emit(todo);
+
   }
 
 }
